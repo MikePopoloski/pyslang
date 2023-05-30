a2436c078e58b2df39fd92257df765b80f475537
Tom Peters <thomas.d.peters@gmail.com>
2023-05-30 12:51:13 -0400

    Add test for visitor pattern
    slang introduced a visitor pattern for Python in 438e922 (Python bindings:
    expose a visitor pattern, Mon May 29 19:36:49 2023 -0400).
    
    Add a test for that feature modeled off tests/unittests/VisitorTests.cpp:
    "Test single counting of statements".

---
+++ tests/test_visitors.py
@@ -0,0 +1,40 @@
+from pyslang import *
+
+
+def test_single_counting_of_statements():
+    """
+    Test the visitor interface in pyslang using a port of slang's
+    tests/unittests/VisitorTests.cpp:"Test single counting of statements".
+    """
+
+    tree = SyntaxTree.fromText(
+        """
+module m;
+    int j;
+    initial begin : asdf
+        j = j + 3;
+        if (1) begin : baz
+            static int i;
+            i = i + 2;
+            if (1) begin : boz
+                i = i + 4;
+            end
+        end
+    end
+endmodule
+    """
+    )
+
+    class Visitor:
+        def __init__(self):
+            self.count = 0
+
+        def visit(self, node):
+            if isinstance(node, Statement):
+                self.count += 1
+
+    c = Compilation()
+    c.addSyntaxTree(tree)
+    v = Visitor()
+    c.getRoot().visit(v.visit)
+    assert v.count == 11
